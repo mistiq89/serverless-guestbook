81b937ca4f9a7e0fbf7f5d135dca843d660e9447
mistiq89 <97790850+mistiq89@users.noreply.github.com>
2022-04-09 14:42:10 +0530

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://11aa2499.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
